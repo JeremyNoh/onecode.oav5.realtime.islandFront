7d1cedcf6e23ab594cd708fdf1cc028690e89c10
madjid yacini <madjid.yacini@gmail.com>
2018-11-19 21:50:46 +0100

    message apparition

--- src/components/MagicNumber/index.js
+++ src/components/MagicNumber/index.js
@@ -1,183 +1,191 @@
-import React, { Component , Fragment} from 'react';
-import './App.css';
-import { Alert ,Button  , toaster, SideSheet,Pane,Heading,Paragraph, Card, TextInput} from 'evergreen-ui'
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import {
+  Alert,
+  Button,
+  toaster,
+  SideSheet,
+  Pane,
+  Heading,
+  Paragraph,
+  Card,
+  TextInput
+} from "evergreen-ui";
 // import io from 'socket.io-client';
-import openSocket from 'socket.io-client';
+import openSocket from "socket.io-client";
 
-
-import Tabs from '../Tabs/index.js'
+import Tabs from "../Tabs/index.js";
 
 class MagicNumber extends Component {
-
-
   constructor(props) {
-  super(props);
-  this.state = {
-    nickname : undefined,
-    start : false,
-    secretNumber : undefined,
-    msgDetail : [],
-    numberTry : 0
+    super(props);
+    this.state = {
+      nickname: undefined,
+      start: false,
+      secretNumber: undefined,
+      msgDetail: [],
+      numberTry: 0
+    };
+    // React.cloneElement(this.props.children, { data: this.state.data, setData: this.updateData })
   }
-  // React.cloneElement(this.props.children, { data: this.state.data, setData: this.updateData })
-}
 
-componentWillMount(){
-  // const socket = io('http://localhost:5000');
-  let { nickname } = this.state
-  if (!(this.props.location.state == undefined)) {
-    nickname = this.props.location.state.nickname.nickname
+  componentWillMount() {
+    // const socket = io('http://localhost:5000');
+    let { nickname } = this.state;
+    if (!(this.props.location.state == undefined)) {
+      nickname = this.props.location.state.nickname.nickname;
+    } else {
+      nickname = "unknownPLayer";
+    }
+    this.setState({ nickname });
+
+    const socket = openSocket("http://localhost:5000/magic");
+    socket.emit("join", nickname);
+    socket.on("welcome", str => {
+      toaster.success(str, {
+        duration: 5
+      });
+    });
+    this.setState({ socket });
   }
-  else{
-    nickname = 'unknownPLayer'
-  }
-  this.setState({ nickname })
 
-  const socket = openSocket('http://localhost:5000/magic');
-  socket.emit("join", nickname)
-  socket.on("welcome", str => {
-    toaster.success(
-       str,
-       {
-         duration: 5
-       }
-     )
-  })
-  this.setState({socket})
-}
+  startTheGame = () => {
+    let { start, socket } = this.state;
 
+    socket.emit("start");
+    socket.on("start", () => {});
+    start = !start;
+    this.setState({ start });
+  };
 
-startTheGame = () => {
+  messageEmit = () => {
+    let { socket, msgDetail } = this.state;
+    let MessageNumber = 0;
+    socket.on("magicNumberMessage", str => {
+      if (MessageNumber < 1) {
+        this.infoForPlayer(str);
+        console.log(this.state.msgDetail);
+        MessageNumber++;
+      }
+    });
+  };
 
-  let { start, socket } = this.state
+  infoForPlayer = str => {
+    let { msgDetail } = this.state;
+    msgDetail.push(str);
+    this.setState({ msgDetail });
+  };
 
-  socket.emit("start")
-  socket.on("start", () => {
+  tryNumber = () => {
+    this.state.socket.emit("beginGame", this.state.numberTry);
+    this.messageEmit();
+  };
 
-  })
-  start = !start
-  this.setState({start})
-}
+  beforeStart = () => {
+    if (!this.state.start) {
+      return (
+        <Pane>
+          <Heading>
+            {this.state.secretNumber === undefined ? "Waiting ..." : `Find me`}
+          </Heading>
+          <Button
+            onClick={() => this.startTheGame()}
+            disabled={this.state.start}
+          >
+            START
+          </Button>
+        </Pane>
+      );
+    }
+  };
 
-messageEmit = () => {
-  let { socket ,msgDetail } = this.state
-  let stre ;
-  socket.on("magicNumberMessage", str => {
-    this.infoForPlayer(str)
-  })
-}
+  inputPlayerUI = () => {
+    if (this.state.start) {
+      return (
+        <Pane>
+          <Heading> Select a number beetween 0 and 1337</Heading>
+          <TextInput
+            label="try your number"
+            required
+            type="number"
+            description="This is for the PLayer..."
+            value={this.state.numberTry}
+            onChange={e => this.setState({ numberTry: e.target.value })}
+          />
+          <Button
+            marginRight={16}
+            appearance="primary"
+            intent="success"
+            onClick={this.tryNumber}
+          >
+            Submit
+          </Button>
+        </Pane>
+      );
+    }
+  };
 
-infoForPlayer = (str) => {
-  let { msgDetail } = this.state
-  msgDetail.push(str)
-  this.setState({msgDetail})
-}
-
-tryNumber = () => {
-  this.state.socket.emit("beginGame",this.state.numberTry)
-  this.messageEmit()
-}
-
-beforeStart = () => {
-  if (!this.state.start) {
-    return(
-      <Pane >
-      <Heading >
-       {this.state.secretNumber === undefined ?"Waiting ..." : `Find me`}
-      </Heading>
-        <Button onClick={() => this.startTheGame()} disabled= {this.state.start}>
-          START
-        </Button>
-       </Pane>
-    )
-  }
-}
-
-
-inputPlayerUI = () => {
-  if (this.state.start) {
+  contentPlay = () => {
     return (
-      <Pane >
-      <Heading> Select a number beetween 0 and 1337</Heading>
-        <TextInput
-          label="try your number"
-          required
-          type = "number"
-          description="This is for the PLayer..."
-          value={this.state.numberTry}
-          onChange={e => this.setState({ numberTry: e.target.value })}
-        />
-        <Button marginRight={16} appearance="primary" intent="success" onClick={this.tryNumber}>Submit</Button>
-       </Pane >
-    )
-  }
-}
-
-
- contentPlay = () =>{
-   return (
-     <Fragment>
-     <SideSheet
-       isShown={this.state.isShown}
-       onCloseComplete={() => this.setState({ isShown: false })}
-       containerProps={{
-         display: 'flex',
-         flex: '1',
-         flexDirection: 'column',
-       }}
-     >
-       <Pane zIndex={1} flexShrink={0} elevation={0} backgroundColor="white">
-         <Pane padding={16}>
-           <Heading size={600}>Welcome to the Game </Heading>
-           <Paragraph size={400}>
-            Rules : generate a random number and you need toguess the number
-           </Paragraph>
-         </Pane>
-       </Pane>
-       <Pane flex="1" overflowY="scroll" background="tint1" padding={16}>
-         <Card
-           backgroundColor="white"
-           elevation={0}
-           height={240}
-           display="flex"
-           alignItems="center"
-           justifyContent="center"
-         >
-          {this.beforeStart()}
-          {this.inputPlayerUI()}
-
-         </Card>
-         {
-           this.state.msgDetail.map(function(item, i){
-             return (
-                   <Alert
-                   key = {i}
-                   appearance="card"
-                   intent="none"
-                   title={"the number is :  "+item}
-                   marginBottom={10}
-                   marginTop={10}
-                 />
-             );
-           })
-         }
-       </Pane>
-     </SideSheet>
-     <Button onClick={() => this.setState({ isShown: true })}>
-       Go PLay
-     </Button>
-   </Fragment>
-   )
- }
+      <Fragment>
+        <SideSheet
+          isShown={this.state.isShown}
+          onCloseComplete={() => this.setState({ isShown: false })}
+          containerProps={{
+            display: "flex",
+            flex: "1",
+            flexDirection: "column"
+          }}
+        >
+          <Pane zIndex={1} flexShrink={0} elevation={0} backgroundColor="white">
+            <Pane padding={16}>
+              <Heading size={600}>Welcome to the Game </Heading>
+              <Paragraph size={400}>
+                Rules : generate a random number and you need toguess the number
+              </Paragraph>
+            </Pane>
+          </Pane>
+          <Pane flex="1" overflowY="scroll" background="tint1" padding={16}>
+            <Card
+              backgroundColor="white"
+              elevation={0}
+              height={240}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              {this.beforeStart()}
+              {this.inputPlayerUI()}
+            </Card>
+            {this.state.msgDetail.map(function(item, i) {
+              return (
+                <Alert
+                  key={i}
+                  appearance="card"
+                  intent="none"
+                  title={"the number is :  " + item}
+                  marginBottom={10}
+                  marginTop={10}
+                />
+              );
+            })}
+          </Pane>
+        </SideSheet>
+        <Button onClick={() => this.setState({ isShown: true })}>
+          Go PLay
+        </Button>
+      </Fragment>
+    );
+  };
 
   render() {
     return (
       <div className="App">
-      <div className="App-header">
-      <Tabs nickname={this.state.nickname} />
-        <p>MagicNumber </p>
-        {this.contentPlay()}
-      </div>
+        <div className="App-header">
+          <Tabs nickname={this.state.nickname} />
+          <p>MagicNumber </p>
+          {this.contentPlay()}
+        </div>
       </div>
     );
   }
